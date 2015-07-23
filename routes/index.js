4a81b8b4aec30c44ce8a2e3125b11773a6478c12
Kepa Santos Ruiz <kepasantosruiz@gmail.com>
2015-07-23 19:29:03 +0200

    Añadido ejercicio obligatorio

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { author: 'Ksantos' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
