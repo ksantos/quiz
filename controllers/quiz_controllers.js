a458cccb5b8a69906377587b13cb7d6357ff7e55
Kepa Santos Ruiz <kepasantosruiz@gmail.com>
2015-08-08 13:29:39 +0200

    Despliegue BD en heroku

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,13 +1,19 @@
+var models = require('../models/models.js');
+
 //get /quizes/question
 exports.question = function(req,res){
-  res.render('quizes/question', {pregunta:'Capital de Italia'});
+  models.Quiz.findAll().then(function(quiz){
+  res.render('quizes/question', {pregunta:quiz[0].pregunta});
+})
 };
 //get /quizes/answer
 exports.answer = function(req,res){
-  if(req.query.respuesta === 'Roma' || req.query.respuesta === 'roma'){
+  models.Quiz.findAll().then(function(quiz){
+
+  if(req.query.respuesta ===  quiz[0].respuesta){
     res.render('quizes/answer', {respuesta:'Correcto'});
   }else{
     res.render('quizes/answer', {respuesta:'Incorrecto'});
   }
-
+})
 };
