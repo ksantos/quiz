2b1a25e54f2b59ff6f2678ea55cd489e3feb71b4
ksantos <kepasantosruiz@gmail.com>
2023-12-19 08:41:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
-//3prueba 236d322w2dd
+//3prueba 236d322w2dd5
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
